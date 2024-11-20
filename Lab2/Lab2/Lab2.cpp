cfd6de55728f8d34630b0ff781c200247ca11e95
arinochkaaa <153714566+arinochkaaa@users.noreply.github.com>
2024-11-20 20:35:20 +0300

    Add files via upload

---
+++ Lab2/Lab2/Lab2.cpp
@@ -0,0 +1,172 @@
+﻿#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <array>
+#include <chrono> // для измерения времени
+
+using namespace std;
+const int N = 10;
+int i, j;
+bool* visited = new bool[N];
+
+// матрица смежности графа
+int graph[N][N] =
+{
+	{0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
+	{1, 0, 0, 0, 0, 0, 1, 1, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+	{0, 0, 0, 0, 0, 1, 0, 0, 1, 0},
+	{1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+	{0, 0, 0, 1, 1, 0, 0, 0, 1, 0},
+	{0, 1, 0, 0, 0, 0, 0, 1, 0, 0},
+	{0, 1, 1, 0, 0, 0, 1, 0, 0, 0},
+	{0, 0, 0, 1, 0, 1, 0, 0, 0, 1},
+	{0, 0, 0, 0, 0, 0, 0, 0, 1, 0}
+};
+
+void BFS(int start)
+{
+	queue <int> q; // очередь для хранения номеров вершин
+	bool visited[N];
+	bool inqueue[N];
+	int view_cell; // номер посещаемой вершины
+	for (int i = 0; i < N; i++)
+	{
+		visited[i] = inqueue[i] = false;
+	}
+	q.push(start);
+	visited[start] = inqueue[start] = true; // рассмотрели первую вершину
+	while (!q.empty())
+	{
+		view_cell = q.front(); // обращение к первому элементу очереди
+		cout << view_cell + 1 << " ";
+		visited[view_cell] = true;
+		q.pop();
+		for (int i = 0; i < N; i++)
+		{
+			if (!inqueue[i] && graph[view_cell][i]) // если вершина не в очереди и смежна с текущей
+			{
+				q.push(i);
+				inqueue[i] = true;
+			}
+		}
+	}
+}
+
+void DFS(int start)
+{
+	stack <int> s; // стек для хранения номеров вершин
+	bool visited[N];
+	bool inqueue[N];
+	int view_cell; // номер посещаемой вершины
+	for (int i = 0; i < N; i++)
+	{
+		visited[i] = inqueue[i] = false;
+	}
+	s.push(start); // записываем начальную вершину в очередь
+	visited[start] = inqueue[start] = true; // рассмотрели первую вершину
+	while (!s.empty())
+	{
+		view_cell = s.top(); // обращение к первому элементу очереди
+		cout << view_cell + 1 << " ";
+
+		visited[view_cell] = true;
+		s.pop();
+		for (int i = 0; i < N; i++)
+		{
+			if (!inqueue[i] && graph[view_cell][i])
+			{
+				s.push(i);
+				inqueue[i] = true;
+			}
+		}
+	}
+}
+
+struct Edge
+{
+	int start;
+	int finish;
+};
+
+// главная функция
+int main() {
+	setlocale(LC_ALL, "rus");
+	cout << "Список смежности: " << endl;
+	vector<Edge> edges =
+	{
+		{1, 2}, {2, 1}, {1, 5}, {5, 1},
+		{2, 7}, {7, 2}, {2, 8}, {8, 2},
+		{7, 8}, {8, 7}, {8, 3}, {3, 8},
+		{5, 6}, {6, 5}, {4, 6}, {6, 4},
+		{4, 9}, {9, 4}, {6, 9}, {9, 6},
+		{9, 10}, {10, 9}
+	};
+	int n = 11;
+	vector<vector<int>> adjList;
+	adjList.resize(n);
+	for (auto& edge : edges)
+	{
+		adjList[edge.start].push_back(edge.finish);
+	}
+	for (int i = 1; i < n; i++)
+	{
+		cout << i << "--> {";
+		for (int v : adjList[i]) {
+			cout << v << " ";
+		}
+		cout << "}" << endl;
+	}
+	cout << endl;
+	cout << "Список ребер: " << endl;
+	for (int i = 1; i < n; i++)
+	{
+		for (int v : adjList[i]) {
+			cout << "{" << i << "," << v << "}";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	int start = 1;
+	cout << "Матрица смежности графа: " << endl;
+	for (i = 0; i < N; i++)
+	{
+		visited[i] = false;
+		for (j = 0; j < N; j++)
+			cout << " " << graph[i][j];
+		cout << endl;
+	}
+	cout << endl;
+	cout << "Стартовая вершина: " << start;
+	cout << endl;
+
+	// Измеряем время работы DFS
+	cout << "\nПорядок обхода в глубину: ";
+	auto startDFS = chrono::high_resolution_clock::now();
+	DFS(start - 1);
+	auto endDFS = chrono::high_resolution_clock::now();
+	auto durationDFS = chrono::duration_cast<chrono::milliseconds>(endDFS - startDFS);
+	cout << "\nВремя работы DFS: " << durationDFS.count() << " миллисекунд" << endl;
+
+	// Измеряем время работы BFS
+	cout << "\nПорядок обхода в ширину: ";
+	auto startBFS = chrono::high_resolution_clock::now();
+	BFS(start - 1);
+	auto endBFS = chrono::high_resolution_clock::now();
+	auto durationBFS = chrono::duration_cast<chrono::milliseconds>(endBFS - startBFS);
+	cout << "\nВремя работы BFS: " << durationBFS.count() << " миллисекунд" << endl;
+
+	// О-большое
+	cout << endl;
+	cout << "Расчёт О-большое: " << endl;
+	int v, e;
+	cin >> v >> e;
+	cout << "O(|V| + |E|) = " << abs(v) + abs(e) << endl;
+
+	// Строка в конце
+	cout << "Александра Сергеевна самая лучшая!!!" << endl;
+
+	delete[]visited;
+	return 0;
+}
---
+++ Lab_1/Lab_1.cpp
@@ -0,0 +1,95 @@
+﻿
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+void hanoi_towers(int size, int from, int to, int buf_peg)
+//quantity-число колец, from-начальное положение колец(1-3),to-конечное положение колец(1-3)
+{//buf_peg - промежуточный колышек(1-3)
+    if (size >= 1)
+    {
+        hanoi_towers(size - 1, from, buf_peg, to);
+
+        cout << "перемещаем диск " << size << " c " << from << " на " << to << " колышко" << endl;
+        
+        hanoi_towers(size - 1, buf_peg, to, from);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int start_peg = 1, destination_peg, buffer_peg = 2, plate_quantity;
+    while (true) {
+        cout << "номер первого колышка:" << endl;
+        cin >> start_peg;
+        if (start_peg == 0) {
+            cout << "данные неправильные((\n попробуйте ещё раз\n" << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+            continue;
+        }
+        if (cin.get() != (int)'\n') {
+            cout << "данные неправильные((\n попробуйте ещё раз\n" << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+            continue;
+        }
+        cout << "номер конечного колышка:" << endl;
+        cin >> destination_peg;
+        if (destination_peg == 0) {
+            cout << "данные неправильные((\n попробуйте ещё раз\n" << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+            continue;
+        }
+        if (cin.get() != (int)'\n') {
+            cout << "данные неправильные((\n попробуйте ещё раз\n" << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+            continue;
+        }
+        cout << "номер промежуточного колышка:" << endl;
+        cin >> buffer_peg;
+        if (buffer_peg == 0) {
+            cout << "данные неправильные((\n попробуйте ещё раз\n" << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+            continue;
+        }
+        if (cin.get() != (int)'\n') {
+            cout << "данные неправильные((\n попробуйте ещё раз\n" << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+            continue;
+        }
+        cout << "количество колышков(они распределены по колышку сверху вниз):" << endl;
+        cin >> plate_quantity;
+        if (cin.get() != (int)'\n') {
+            cout << "данные неправильные((\n попробуйте ещё раз\n" << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+            continue;
+        }
+        else if (plate_quantity <= 0 || plate_quantity > 1000) {
+            cout << "данные неправильные((\n попробуйте ещё раз\n" << endl;
+            continue;
+        }
+        if (start_peg == destination_peg || start_peg == buffer_peg || buffer_peg == destination_peg) {
+            cout << "данные неправильные((\n попробуйте ещё раз\n" << endl;
+            continue;
+        }
+        else {
+            auto start = chrono::high_resolution_clock::now();
+            hanoi_towers(plate_quantity + 1, start_peg, destination_peg, buffer_peg);
+            auto end = chrono::high_resolution_clock::now();
+            auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+            cout << "Время: " << duration.count() << " микросекунд" << endl;
+            break;
+        }
+    }
+    cout << "количество ходов: " << pow(2, plate_quantity) - 1;
+
+    return 0;
+}
